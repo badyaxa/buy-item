0e08bf8dfffcbced593227132ab2ecc99ba9d464
badyaxa <badyaxa@gmail.com>
2019-04-14 23:52:28 +0300

    2019/04/14 23/52

---
+++ src/main/java/bohdan/stasiv/buyitem/controller/AdminHomeController.java
@@ -0,0 +1,20 @@
+package bohdan.stasiv.buyitem.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@CrossOrigin
+@Controller
+@RequestMapping("/admin")
+public class AdminHomeController {
+
+    private String htmlPagePath = "admin/";
+
+    @GetMapping("/")
+    public String index() {
+        return htmlPagePath + "index";
+    }
+
+}
